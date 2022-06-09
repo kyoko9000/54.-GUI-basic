1ed892da70592e9f5248aab33178aea71dd34d38
lato' channel <88913272+kyoko9000@users.noreply.github.com>
2022-06-09 20:56:33 +0700

    Update gui3.py

--- gui3.py
+++ gui3.py
@@ -14,21 +14,26 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(572, 440)
+        MainWindow.resize(436, 366)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.gridLayout = QtWidgets.QGridLayout(self.centralwidget)
-        self.gridLayout.setObjectName("gridLayout")
+        self.horizontalLayout = QtWidgets.QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName("horizontalLayout")
         self.listWidget = QtWidgets.QListWidget(self.centralwidget)
-        self.listWidget.setStyleSheet("background-color: rgb(75, 75, 75);")
-        self.listWidget.setObjectName("listWidget")
-        self.gridLayout.addWidget(self.listWidget, 0, 0, 1, 1)
-        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
         font = QtGui.QFont()
-        font.setPointSize(28)
-        self.pushButton.setFont(font)
-        self.pushButton.setObjectName("pushButton")
-        self.gridLayout.addWidget(self.pushButton, 1, 0, 1, 1)
+        font.setPointSize(20)
+        self.listWidget.setFont(font)
+        self.listWidget.setStyleSheet("QListWidget {\n"
+"      background-color: rgb(46, 46, 46);\n"
+"}\n"
+"QListWidget:hover {\n"
+"      background-color: rgb(50, 50, 50);\n"
+"}\n"
+"QListWidget:select {\n"
+"      background-color: rgb(0, 20, 163);\n"
+"}")
+        self.listWidget.setObjectName("listWidget")
+        self.horizontalLayout.addWidget(self.listWidget)
         MainWindow.setCentralWidget(self.centralwidget)
 
         self.retranslateUi(MainWindow)
@@ -37,7 +42,6 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.pushButton.setText(_translate("MainWindow", "PushButton"))
 
 
 if __name__ == "__main__":
